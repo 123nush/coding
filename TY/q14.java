8e5d9159c52977972322c6682ca42217a8c5e68f
123nush <119521757+123nush@users.noreply.github.com>
2023-01-03 22:33:51 +0530

    Update q14.java

--- TY/q14.java
+++ TY/q14.java
@@ -2,14 +2,16 @@ import java.util.*;
 
 public class q14 {
     public static void main(String[] args) {
-        Scanner input = new scanner(System.in);
+        Scanner input = new scanner(System.in);//Scanner input = new Scanner(System.in);
         System.out.println("Enter a number: ");
         int num = input.nextInt();
 
         if (num > 0) {
             System.out.println(num + " is positive.");
+            //}
         else if (num < 0) {
             System.out.println(num + " is negative.");
+            //}
         else {
             System.out.println(num + " is zero.");
         }
